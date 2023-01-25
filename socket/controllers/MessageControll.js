888607a4ff26358a4b7af82bb3d0e13fbc321256
Maor <maorgabaynew@gmail.com>
2023-01-25 15:22:09 +0200

    d

--- socket/controllers/MessageControll.js
+++ socket/controllers/MessageControll.js
@@ -3,6 +3,7 @@ import BaseController from "./BaseController.js";
 export default class MessageController extends BaseController {
 
     sendMessage = ({ message, roomId }) => {
+        console.log("sendMessage")
         let skt = this.socket.broadcast;
         skt = roomId ? skt.to(roomId) : skt;
         skt.emit('message-from-server', { message });
